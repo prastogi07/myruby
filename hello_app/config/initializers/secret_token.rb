693ede3527447c23321337b473d75a9c639a1185
Pooja Rastogi <prastogi@Poojas-MBPro-15.local>
2015-05-05 09:57:58 -0700

    created sample ruby application

---
+++ hello_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloApp::Application.config.secret_token = '75e52499793d84a2b1f1f9fc6b936bfe23c5abbbda7d966a725fdc17d380ebe99278034ff4f4df6185d67c7d370f8c5d4c6ccc740f33dd91d52187eb341f7ea3'
